49633ee8066a675d38a56adc1b411b9da45f4988
ZansNotLord <165402041+ZansNotLord@users.noreply.github.com>
2024-07-31 23:50:08 +0700

    Update Footer.jsx

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -20,13 +20,13 @@ const Footer = () => {
 							<h2 class="mb-4 text-sm font-semibold uppercase text-white">CREATED BY</h2>
 							<ul class="text-gray-400 dark:text-gray-400 font-medium">
 								<li class="mb-2">
-									<a href="https://www.instagram.com/ekizr_/?hl=id" class="hover:underline">
-										EKIZR
+									<a href="https://www.instagram.com/zans.not.lords/?hl=id" class="hover:underline">
+										ZansLord
 									</a>
 								</li>
 								<li>
-									<a href="https://www.instagram.com/dafyall/?hl=id" class="hover:underline">
-										DAFYALL
+									<a href="https://www.instagram.com/x_pplg2sevbe/?hl=id" class="hover:underline">
+										X PPLG 2
 									</a>
 								</li>
 							</ul>
@@ -35,13 +35,13 @@ const Footer = () => {
 							<h2 class="mb-4 text-sm font-semibold uppercase text-white">Follow us</h2>
 							<ul class="text-gray-400 dark:text-gray-400 font-medium">
 								<li class="mb-2">
-									<a href="https://www.instagram.com/acetkjthree/?hl=id" class="hover:underline ">
-										Instagram
+									<a href="https://www.instagram.com/x_pplg2sevbe/?hl=id" class="hover:underline ">
+										Instagram Kelas
 									</a>
 								</li>
 								<li>
-									<a href="https://www.youtube.com/@11TKJ3" class="hover:underline">
-										Youtube
+									<a href="https://www.instagram.com/smkn7.baleendah" class="hover:underline">
+										Instagram Sekolah
 									</a>
 								</li>
 							</ul>
@@ -60,7 +60,7 @@ const Footer = () => {
 				<div class="sm:flex sm:items-center sm:justify-between">
 					<div class="flex mt-4 justify-center sm:mt-0">
 						<p className="text-[0.7rem] text-white opacity-70">
-							© {new Date().getFullYear()} Kelas XII TKJ 3 | Di Kelola Oleh Siswa TKJ 3
+							© {new Date().getFullYear()} Kelas X PPLG 2 | Di Kelola Oleh Siswa PPLG 2
 						</p>
 					</div>
 				</div>
